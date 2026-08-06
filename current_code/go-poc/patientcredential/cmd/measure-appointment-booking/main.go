c2e8427a6069213145906edf55aa38bd95b7d197
Nasif Muslim <nasif2005@gmail.com>
2026-08-06 16:03:02 -0400

    Add current Go HIDM proof of concept

---
+++ current_code/go-poc/patientcredential/cmd/measure-appointment-booking/main.go
@@ -0,0 +1,34 @@
+// Command measure-appointment-booking writes repeatable appointment-booking
+// latency, communication, and storage measurements as JSON.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	pc "github.com/nasif2005/healthcare-specific-idm/go-poc/patientcredential"
+)
+
+func main() {
+	iterations := flag.Int("iterations", 30, "number of measured workflow repetitions")
+	output := flag.String("output", "", "optional JSON output file; defaults to standard output")
+	flag.Parse()
+	result, err := pc.MeasureAppointmentBooking(*iterations)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *output == "" {
+		fmt.Println(string(data))
+		return
+	}
+	if err = os.WriteFile(*output, append(data, '\n'), 0o600); err != nil {
+		log.Fatal(err)
+	}
+}
